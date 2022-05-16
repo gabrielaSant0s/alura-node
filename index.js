d8814b36231b0d07c3b693631cfcc015bd250945
Gabriela S Santos <gbss.santos@poli.ufrj.br>
2022-05-16 13:43:02 -0300

    método assíncrono 2

--- index.js
+++ index.js
@@ -18,12 +18,25 @@ function trataErro(erro) {
 // }
 
 // metodo assíncrono de ler arquivos
-function pegaArquivo(caminhoDoArquivo) {
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf8";
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(texto))
+//     .catch((erro) => trataErro(erro));
+// }
+
+// metodo assincrono async await
+async function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf8";
-  fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(texto))
-    .catch((erro) => trataErro(erro));
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro);
+  } finally {
+    console.log(chalk.yellow("operação concluída"));
+  }
 }
 
 pegaArquivo("./arquivos/texto1.md");
